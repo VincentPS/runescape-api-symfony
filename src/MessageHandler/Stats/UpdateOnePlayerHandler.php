ad71ecb6707cc08a68ff61e2f0b83c0256e30bb6
VincentPS <schaddelee.vincent@gmail.com>
2025-02-10 18:40:33 +0100

    Catch the nullable player name which can happen when a player isn't available anymore from the API

--- src/MessageHandler/Stats/UpdateOnePlayerHandler.php
+++ src/MessageHandler/Stats/UpdateOnePlayerHandler.php
@@ -6,28 +6,37 @@ use App\Exception\PlayerApi\PlayerApiDataConversionException;
 use App\Exception\PlayerApi\PlayerNotAMemberException;
 use App\Exception\PlayerApi\PlayerNotFoundException;
 use App\Message\Stats\UpdateOnePlayerMessage;
+use App\Repository\KnownPlayerRepository;
 use App\Service\RsApiService;
+use Doctrine\ORM\EntityManagerInterface;
 use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
-use Symfony\Component\Messenger\Exception\ExceptionInterface;
 
 #[AsMessageHandler]
 final readonly class UpdateOnePlayerHandler
 {
     public function __construct(
-        private RsApiService $rsApiService
+        private RsApiService $rsApiService,
+        private KnownPlayerRepository $knownPlayerRepository,
+        private EntityManagerInterface $entityManager
     ) {
     }
 
     /**
      * @throws GuzzleException
-     * @throws PlayerApiDataConversionException
-     * @throws PlayerNotAMemberException
-     * @throws PlayerNotFoundException
-     * @throws ExceptionInterface
      */
     public function __invoke(UpdateOnePlayerMessage $message): void
     {
-        $this->rsApiService->getProfile($message->player);
+        try {
+            $this->rsApiService->getProfile($message->player);
+        } catch (PlayerNotFoundException | PlayerNotAMemberException | PlayerApiDataConversionException) {
+            // check if player is a KnownPlayer and remove it because it can't be found anymore
+            $knownPlayer = $this->knownPlayerRepository->findOneByName($message->player);
+
+            if ($knownPlayer !== null) {
+                $this->entityManager->remove($knownPlayer);
+                $this->entityManager->flush();
+            }
+        }
     }
 }
