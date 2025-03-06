f6e6ec620808b6689be41274cb8a2c2c7ad493c4
VincentPS <schaddelee.vincent@gmail.com>
2025-03-06 20:55:36 +0100

    Corrected the double xp detection

--- src/Service/DoubleXpService.php
+++ src/Service/DoubleXpService.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use App\Trait\GuzzleCachedClientTrait;
+use DateTimeImmutable;
 use GuzzleHttp\Exception\GuzzleException;
 
 class DoubleXpService
@@ -12,10 +13,42 @@ class DoubleXpService
     public function isDoubleXpLive(): bool
     {
         try {
-            $response = $this->getClient()->get('https://www.runescape.com/community/double-xp-live');
+            $response = $this->getClient()->get('https://rs.runescape.com/en-GB/double-xp');
             $content = $response->getBody()->getContents();
 
-            return str_contains($content, 'Active - ends on');
+            preg_match('/<script id="__NEXT_DATA__" type="application\/json">(.*?)<\/script>/s', $content, $matches);
+
+            if (isset($matches[1])) {
+                /**
+                 * @var null|array{
+                 *     props: array{
+                 *          pageProps: array{
+                 *              doubleXpDetails: array{
+                 *                  endDateTime: string
+                 *              }
+                 *          }
+                 *     }
+                 * } $jsonData
+                 */
+                $jsonData = json_decode($matches[1], true);
+
+                if ($jsonData === null) {
+                    return false;
+                }
+
+                if (!empty($jsonData['props']['pageProps']['doubleXpDetails']['endDateTime'])) {
+                    $endDateTime = DateTimeImmutable::createFromFormat(
+                        'Y-m-d\TH:i:s\Z',
+                        $jsonData['props']['pageProps']['doubleXpDetails']['endDateTime']
+                    );
+
+                    if ($endDateTime instanceof DateTimeImmutable && $endDateTime > new DateTimeImmutable()) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         } catch (GuzzleException) {
             return false;
         }
